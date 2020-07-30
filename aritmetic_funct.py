41fb988e5483555cf515ade99a116ecf5a0c25a9
denb11 <68423818+denb11@users.noreply.github.com>
2020-07-30 22:27:42 +0300

    Add files via upload

---
+++ aritmetic_funct.py
@@ -0,0 +1,9 @@
+
+def add(a, b ):
+    if type(a) == int and type(b) == int:
+        result = a + b
+        print(result)
+    else:
+        print("valorile transmise nu corespund tipului")
+
+add(10, 20)
